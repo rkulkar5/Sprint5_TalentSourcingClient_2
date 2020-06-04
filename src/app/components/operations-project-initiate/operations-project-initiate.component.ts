c42d650a457e7c15f6399c925bf1530ae4dc41fe
Ranjeet Kumar <ranjeku5@in.ibm.com>
2020-06-05 01:36:34 +0530

    Added code for operation stories

--- src/app/components/operations-project-initiate/operations-project-initiate.component.ts
+++ src/app/components/operations-project-initiate/operations-project-initiate.component.ts
@@ -11,8 +11,9 @@ import { browserRefresh } from '../../app.component';
 })
 export class OperationsProjectInitiateComponent implements OnInit {
   userName: String = "";
-  operationsProjectDetails : any = [];  
-  operationsFeedbackForm: FormGroup;
+  operationsProjectDetails : any = []; 
+  ProjectLocation: any=['Onshore','Offshore']; 
+  operationsProjectForm: FormGroup;
   submitted = false;
   formReset = false;
 
@@ -20,11 +21,32 @@ export class OperationsProjectInitiateComponent implements OnInit {
   private apiService: ApiService) {
        this.userName = this.router.getCurrentNavigation().extras.state.username;
        let id = this.actRoute.snapshot.paramMap.get('id');
-       this.readOperationsProjectDetails(id);      
+       this.readOperationsProjectDetails(id); 
+       this.mainForm();     
    }
 
   ngOnInit(): void {
   } 
+
+  mainForm() {
+    this.operationsProjectForm = this.fb.group({
+      projectLocation: ['', [Validators.required]],
+      projectName: ['', [Validators.required]],
+      projectPosition: ['', [Validators.required]]
+    })
+}
+
+get myForm(){
+  return this.operationsProjectForm.controls;
+}
+
+  // Choose Location with select dropdown
+  updateLocation(e){
+    console.log("Inside updateLocation="+e);
+    this.operationsProjectForm.get('projectLocation').setValue(e, {
+    onlySelf: true
+    })
+  }
 
   //Read candidate project details
   readOperationsProjectDetails(id) {
@@ -32,4 +54,21 @@ export class OperationsProjectInitiateComponent implements OnInit {
       this.operationsProjectDetails = data;
     });
   }
+
+  onSubmit(id) {
+    this.submitted = true;
+
+    // TO-DO - Code to insert/update the value to ProjectAlloc table
+
+}
+
+//Reset
+resetForm(){
+ this.formReset = true;
+ this.operationsProjectForm.reset();
+}
+//Cancel
+cancelForm(){
+  this.ngZone.run(() => this.router.navigateByUrl('/operations-candidate-list',{state:{username:this.userName}}))
+}
 }
