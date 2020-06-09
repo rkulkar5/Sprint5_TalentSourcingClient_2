eb40f72d0e8451645c382825aebd057e638cd500
Adharshd <adharsh2208@gmail.com>
2020-06-09 21:57:04 +0530

    workflow config updates

---
+++ src/app/components/workflow-config/workflow-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workflow-config',
+  templateUrl: './workflow-config.component.html',
+  styleUrls: ['./workflow-config.component.css']
+})
+export class WorkflowConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
