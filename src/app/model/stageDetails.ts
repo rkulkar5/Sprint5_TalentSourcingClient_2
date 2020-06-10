bb243843d694c5b3c283be4994322964e8ad81e6
rkulkar5 <rkulkar5@in.ibm.com>
2020-06-10 14:00:09 +0530

    udated the stage2_Status variable name

--- src/app/model/stageDetails.ts
+++ src/app/model/stageDetails.ts
@@ -1,16 +1,16 @@
 export class stageDetails {
-     public stage1_Status: any;
-     public stage2_Status: any;
-	 public stage3_Status: any;
-	 public stage4_Status: any;
-	 public stage5_Status: any;
+     public stage1_status: any;
+     public stage2_status: any;
+	 public stage3_status: any;
+	 public stage4_status: any;
+	 public stage5_status: any;
 
-  constructor( stage1_Status, stage2_Status,  stage3_Status,  stage4_Status,  stage5_Status) {
-        this.stage1_Status = stage1_Status;
-		this.stage2_Status = stage2_Status;
-		this.stage3_Status = stage3_Status;
-		this.stage4_Status = stage4_Status;
-		this.stage5_Status = stage5_Status;
+  constructor( stage1_status, stage2_status,  stage3_status,  stage4_status,  stage5_status) {
+        this.stage1_status = stage1_status;
+		this.stage2_status = stage2_status;
+		this.stage3_status = stage3_status;
+		this.stage4_status = stage4_status;
+		this.stage5_status = stage5_status;
     }
     
 }
