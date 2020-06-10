b220a2c14986127cf69941c4f042d95bd1477ced
rkulkar5 <rkulkar5@in.ibm.com>
2020-06-10 11:19:04 +0530

    Create stageDetails.ts

---
+++ src/app/model/stageDetails.ts
@@ -0,0 +1,17 @@
+export class stageDetails {
+     public stage1_Status: any;
+     public stage2_Status: any;
+	 public stage3_Status: any;
+	 public stage4_Status: any;
+	 public stage5_Status: any;
+
+  constructor( stage1_Status, stage2_Status,  stage3_Status,  stage4_Status,  stage5_Status) {
+        this.stage1_Status = stage1_Status;
+		this.stage2_Status = stage2_Status;
+		this.stage3_Status = stage3_Status;
+		this.stage4_Status = stage4_Status;
+		this.stage5_Status = stage5_Status;
+    }
+    
+}
+
